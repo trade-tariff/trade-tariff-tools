18947ebfb88e0d00e40f8de5a39d4cccf0df4be6
William Fish <william.michael.fish@gmail.com>
2023-02-09 12:38:34 +0000

    HOTT-2650: Automates release note generation

---
+++ .github/pull_request_template.md
@@ -0,0 +1,23 @@
+### Jira link
+
+HOTT-<TODO>
+
+### What?
+
+I have added/removed/altered:
+
+- [ ] Added foo in bar
+- [ ] Removed baz
+
+### Why?
+
+I am doing this because:
+
+-
+-
+-
+
+### Have you? (optional)
+
+- [ ] Clearly documented/self-documented any scripts I've added
+- [ ] Respected people's right not to receive lots of noisy messages
---
+++ bin/post_release_notes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+webhook_url="$SLACK_WEBHOOK"
+channel="#$SLACK_CHANNEL"
+username="$SLACK_USERNAME"
+message=$(bash bin/generate_release_notes.sh)
+payload="{
+  \"channel\": \"$channel\",
+  \"text\": \"$message\",
+  \"username\": \"$username\",
+  \"mrkdwn\": true
+}"
+
+curl -X POST -H "Content-type: application/json" --data "$payload" "$webhook_url"
