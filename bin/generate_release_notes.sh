9a23238c3e8705525d5669da28b786b750abae15
William Fish <william.michael.fish@gmail.com>
2025-03-12 16:01:17 +0000

    Fixes release notes

--- bin/generate_release_notes.sh
+++ bin/generate_release_notes.sh
@@ -28,20 +28,12 @@ repos=(
   "https://github.com/trade-tariff/trade-tariff-duty-calculator.git"
   "https://github.com/trade-tariff/trade-tariff-admin.git"
   "https://github.com/trade-tariff/trade-tariff-api-docs.git"
-  "https://github.com/trade-tariff/trade-tariff-testing.git"
-  "https://github.com/trade-tariff/process-appendix-5a.git"
-  "https://github.com/trade-tariff/download-CDS-files.git"
   "https://github.com/trade-tariff/trade-tariff-platform-aws-terraform.git"
   "https://github.com/trade-tariff/trade-tariff-platform-terraform-modules.git"
-  "https://github.com/trade-tariff/trade-tariff-reporting.git"
   "https://github.com/trade-tariff/trade-tariff-tech-docs.git"
   "https://github.com/trade-tariff/trade-tariff-fpo-dev-hub-e2e.git"
   "https://github.com/trade-tariff/trade-tariff-lambdas-fpo-search.git"
-  "https://github.com/trade-tariff/trade-tariff-dev-hub-frontend.git"
-  "https://github.com/trade-tariff/trade-tariff-dev-hub-backend.git"
   "https://github.com/trade-tariff/trade-tariff-lambdas-fpo-model-garbage-collection"
-  "https://github.com/trade-tariff/trade-tariff-lambdas-database-replication"
-  "https://github.com/trade-tariff/trade-tariff-commodi-tea"
 )
 
 for repo in "${repos[@]}"; do
@@ -57,68 +49,7 @@ fi
 
 cache_file="$PWD/.github_authors_cache"
 
-function fetch_build_status() {
-  local repo="$1"
-  local pr_number="$2"
-  pr_sha=$(curl -s -H "$auth_header" "https://api.github.com/repos/trade-tariff/$repo/pulls/$pr_number" | jq -r '.head.sha')
-
-  commit_status=$(curl -s -H "$auth_header" "https://api.github.com/repos/trade-tariff/$repo/commits/$pr_sha/status")
-
-  if echo "$commit_status" | grep -q "API rate limit exceeded"; then
-    echo ":question: Build"
-  else
-    build_status=$(echo "$commit_status" | jq '.statuses')
-
-    if [[ "$build_status" == "" || "$build_status" == "[]" ]]; then
-      echo "No builds"
-    else
-      unique_statuses=("$(echo "$build_status" | jq -r '.[].state' | sort -u)")
-
-      if [[ "${unique_statuses[*]}" == "success" ]]; then
-        echo ":white_check_mark: Build"
-      else
-        echo ":x: Build"
-      fi
-    fi
-  fi
-}
-
-function fetch_approval_status() {
-  local repo="$1"
-  local pr_number="$2"
-
-  reviews=$(curl -s -H "$auth_header" "https://api.github.com/repos/trade-tariff/${repo}/pulls/${pr_number}/reviews")
-
-  if echo "$reviews" | grep -q "API rate limit exceeded"; then
-    echo ":question: approved"
-  else
-    approved_reviews=$(echo "$reviews" | jq -r '.[] | select(.state == "APPROVED")' | jq -s)
-    changes_requested_reviews=$(echo "$reviews" | jq -r '.[] | select(.state == "CHANGES_REQUESTED" and .user.login != "dependabot[bot]")')
-    num_approved_reviews=$(echo "$approved_reviews" | jq -r 'map(.user.login) | unique | length')
-
-    if [[ "$reviews" == "" || "$reviews" == "[]" ]]; then
-      echo "No reviews"
-    elif [[ "$approved_reviews" == "" ]]; then
-      echo ":x: Not approved"
-    elif [[ "$changes_requested_reviews" != "" ]]; then
-      echo ":x: Changes requested"
-    else
-      echo ":white_check_mark: Approved (${num_approved_reviews})"
-    fi
-  fi
-}
-
-function check_pr_status() {
-  local repo="$1"
-  local pr_number="$2"
-
-  build_status=$(fetch_build_status "$repo" "$pr_number")
-  approval_status=$(fetch_approval_status "$repo" "$pr_number")
-
-  echo "${build_status} ${approval_status}"
-}
-
-cachedFetchAuthor() {
+function cached_fetch_author() {
   local email="$1"
 
   result=$(grep "^$email," "$cache_file")
@@ -170,10 +101,9 @@ function print_merge_logs() {
       message=$(echo "$line" | awk -F\| '{print $1}')
       subject_line=$(echo "$line" | awk -F\| '{print $2}')
       email=$(echo "$line" | awk -F\| '{print $3}')
-      username=$(cachedFetchAuthor "$email")
+      username=$(cached_fetch_author "$email")
       pr_number=$(echo "$subject_line" | sed 's/^Merge pull request #\([0-9]*\).*$/\1/g')
       pr_link="https://github.com/trade-tariff/${repo}/pull/${pr_number}"
-      # pr_status="$(check_pr_status "$repo" "$pr_number")"
 
       echo "- <${pr_link}|${message}> by ${username}"
     done <<<"$merge_commits"
@@ -220,20 +150,12 @@ all_logs() {
   log_for "https://www.trade-tariff.service.gov.uk/duty-calculator/healthcheck" "trade-tariff-duty-calculator" "manual"
   log_for "https://admin.trade-tariff.service.gov.uk/healthcheck" "trade-tariff-admin" "manual"
   last_n_logs_for "trade-tariff-api-docs" "continuous"
-  last_n_logs_for "trade-tariff-testing" "n/a"
-  last_n_logs_for "process-appendix-5a" "continuous"
-  last_n_logs_for "download-CDS-files" "n/a"
   last_n_logs_for "trade-tariff-platform-aws-terraform" "manual"
   last_n_logs_for "trade-tariff-platform-terraform-modules" "n/a"
-  last_n_logs_for "trade-tariff-reporting" "continuous"
   last_n_logs_for "trade-tariff-tech-docs" "continuous"
   last_n_logs_for "trade-tariff-fpo-dev-hub-e2e" "continuous"
   last_n_logs_for "trade-tariff-lambdas-fpo-search" "continuous"
   last_n_logs_for "trade-tariff-lambdas-fpo-model-garbage-collection" "continuous"
-  last_n_logs_for "trade-tariff-dev-hub-frontend" "continuous"
-  last_n_logs_for "trade-tariff-dev-hub-backend" "continuous"
-  last_n_logs_for "trade-tariff-lambdas-database-replication" "continuous"
-  last_n_logs_for "trade-tariff-commodi-tea" "continuous"
 }
 
 all_logs
