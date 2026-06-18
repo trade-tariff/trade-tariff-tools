23c3832eef7100ce9278635144bca3337e9e9d6a
William Fish <william.michael.fish@gmail.com>
2026-06-18 21:04:39 +0100

    chore: wire nix pre-commit hooks
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,13 +2,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix/3bbec39bc90eadfa031e6f3b77272f3f60803e39";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      pre-commit-hooks,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -39,10 +44,96 @@
             license = licenses.mit;
           };
         };
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            actionlint = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-added-large-files = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-case-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-merge-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            detect-private-keys = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              package = pre-commit-hooks.inputs.nixpkgs.legacyPackages.${system}.nixfmt;
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            shellcheck = {
+              enable = true;
+              args = [ "--severity=error" ];
+              stages = [ "pre-commit" ];
+            };
+            statix = {
+              enable = true;
+              settings.ignore = [ "{.direnv,.nix,.worktrees}/**" ];
+              stages = [ "pre-commit" ];
+            };
+            trufflehog = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+
+        preCommit = pkgs.writeShellScriptBin "pre-commit" ''
+          set -euo pipefail
+
+          has_config=false
+          for arg in "$@"; do
+            case "$arg" in
+              -c|--config|--config=*)
+                has_config=true
+                ;;
+            esac
+          done
+
+          if [ "$has_config" = true ]; then
+            exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+          fi
+
+          if [ "''${1:-}" = "run" ]; then
+            shift
+            exec ${preCommitCheck.config.package}/bin/pre-commit run --config .pre-commit-config-nix.yaml "$@"
+          fi
+
+          exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+        '';
       in
       {
         packages.default = ecs;
         packages.ecs = ecs;
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${preCommitCheck.shellHook}
+            export PATH=${preCommit}/bin:$PATH
+          '';
+
+          buildInputs = preCommitCheck.enabledPackages ++ runtimeDeps;
+        };
       }
     );
 }
